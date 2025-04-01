78cc8a1e3a09e6d7d69bb8547bc798a9f0130fe7
debayudh2001 <debayudh31@gmail.com>
2025-04-01 17:12:43 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React, {Component, useState} from "react";
+import React, {Component, useRef, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const para = useRef(null);
+  function handleClick(){
+    para.current.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>Click Me</button>
+      <p id="para" ref={para}></p>
     </div>
   );
 }
